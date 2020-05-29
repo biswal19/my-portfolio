8f1bce713c9c889ef4fe254eb338aa1d89d644ec
chandrika <chandrikabiswal1@gmail.com>
2020-05-29 22:29:41 +0530

    Updates

---
+++ precache-manifest.34f6a0fcc306523ac0bf33877c6b4585.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191d0ee08034b93fbfeba996660e7447",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e6c049d06face54a4397",
+    "url": "/my-portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "63c1d98aee43fcf8d97f",
+    "url": "/my-portfolio/static/css/main.bf496449.chunk.css"
+  },
+  {
+    "revision": "e6c049d06face54a4397",
+    "url": "/my-portfolio/static/js/2.9dada157.chunk.js"
+  },
+  {
+    "revision": "cf7302ab57415842ba2bf20f719dee93",
+    "url": "/my-portfolio/static/js/2.9dada157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c1d98aee43fcf8d97f",
+    "url": "/my-portfolio/static/js/main.6fb388d7.chunk.js"
+  },
+  {
+    "revision": "0b08248d58d0841f9a53",
+    "url": "/my-portfolio/static/js/runtime-main.6db96563.js"
+  },
+  {
+    "revision": "6be2a78fd6afaf88b124bd95ff73d26e",
+    "url": "/my-portfolio/static/media/0001.6be2a78f.jpg"
+  },
+  {
+    "revision": "608217a0386833a1bd42b5a77abbabb1",
+    "url": "/my-portfolio/static/media/0002.608217a0.jpg"
+  },
+  {
+    "revision": "3cbbe1da3134a13b544047b3fb19cc55",
+    "url": "/my-portfolio/static/media/sonali.3cbbe1da.jpg"
+  }
+]);
